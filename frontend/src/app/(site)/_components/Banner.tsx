852207ef2ce113ed96ca7e99fc694de53d6e078d
DanylloRangel <danyllorangeldeazevedo@gmail.com>
2026-04-08 20:15:17 -0300

    feat: adiciona carrossel na interface utilizando a biblioteca Swiper

--- frontend/src/app/(site)/_components/Banner.tsx
+++ frontend/src/app/(site)/_components/Banner.tsx
@@ -1,10 +1,39 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./Banner.module.css";
+import {} from 'swiper/element/bundle';
+import 'swiper/css/bundle';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 export default function Banner() {
   return (
     <section className={styles.container}>
-      <Image className={styles.bannerImage} src="/assets/images/banner1.png" alt="Banner" fill />
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        navigation={true}
+      >
+        <SwiperSlide>
+          <div className={styles.container}>
+            <Image className={styles.bannerImage} src="/assets/images/banner1.png" alt="Banner" fill />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.container}>
+            <Image className={styles.bannerImage} src="/assets/images/banner1.png" alt="Banner" fill />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.container}>
+            <Image className={styles.bannerImage} src="/assets/images/banner1.png" alt="Banner" fill />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
